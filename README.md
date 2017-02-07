3e6632bb5538e1af4935a8e85e0412c251dceaf9
Jacob Lo <jl.jacoblo@gmail.com>
2017-02-06 19:47:24 -0600

    app-route iron-page started2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PolymerGetStarted2
 This project trying to re-create Polymer Shop project from scratch.
+This will cause a lot of time, but it is better understand polymer concept is the have a control environment ( the polymer SHop project ),
+and work the way through it by coding bit by bit and hopefully i will be as good as the ones that create the shop project
+
+This product probably will not as same as the original Polymer shop project but it will be( hopefully if i do i good job ) very close.
--- src/shop-app/shop-app.js
+++ src/shop-app/shop-app.js
@@ -1,18 +1,31 @@
 Polymer({
-        is : "shop-app"
+  is: "shop-app"
 
-        // class members
-        ,properties : {
-            page : {
-                type : String
-                ,reflectToAttribute : true
-                ,value : 'home'
-                ,observer : '_pageChanged'
-            }
-        }
+  // class members
+  , properties: {
+    page: {
+      type: String
+      , reflectToAttribute: true
+      , value: 'home'
+      , observer: '_pageChanged'
+    }
+  }
 
-        // methods
-        ,_pageChanged : function (page, oldPage) {
+  // observers
+  , observers : [
+    '_routePageChanged(routeData.page)'
+  ]
 
-        }
-    });
+  // methods
+  , _pageChanged: function (page, oldPage) {
+    this.importHref(
+      this.resolveUrl("../shop-" + page + "/shop-" + page + ".html")
+      , () => {console.log('Page : ' + page + ' lazy loaded');}
+      , () => {console.log('_pageChanged() Page : ' + page + ' error!');}
+      , true );
+  }
+
+  , _routePageChanged(page) {
+      this.page = page || 'home';
+  }
+});
