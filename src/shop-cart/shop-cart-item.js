1129a9df94844fb2c8c83d1a45c330d6ba018663
JMacbook@jacoblo.net <JMacbook@jacoblo.net>
2017-02-21 22:35:44 -0600

    almost finish shop-cart-item, still cannot change input box value

--- src/shop-cart/shop-cart-item.js
+++ src/shop-cart/shop-cart-item.js
@@ -6,17 +6,43 @@ Polymer ({
   }
 
   , _setCartItem : function (quantity) {
+    var i = Number(quantity);
+    if (i < 0) {
+      i = 1;
+    }
     this.fire('set-cart-item', {
       item : this.entry.item
-      , quantity : quantity
+      , quantity : i
       , size : this.entry.size
     });
   }
-  , _removeItem : function () {
+
+  , _addItem : function () {
+    this._setCartItem(this.entry.quantity+1);
+  }
+
+  , _removeOneItem : function () {
+    this._setCartItem(this.entry.quantity-1);
+  }
+
+  , _removeItems : function () {
     this._setCartItem(0);
   }
 
-  , _quantityChange : function () {
-    this._setCartItem(parseInt(this.$.quantitySelect.value, 10));
+  // TODO : still change really correct
+  , _itemQuantityChanged : function () {
+    var i = Number(this.$.inputQuantity.value);
+    if (this.isPositiveInteger(i)) {
+      this._setCartItem(i);
+    }
+    else {
+      this.$.inputQuantity.value = this.entry.quantity;
+    }
   }
+
+  , isPositiveInteger : function(str) {
+    var n = Math.floor(Number(str));
+    return n === String(n) && n > 0;
+  }
+
 });
